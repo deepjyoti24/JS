a0d6500c864e35b20554d77101c73684cbc9556b
Deepjyoti Kalita <kdeepjyoti311@gmail.com>
2024-03-02 12:27:14 +0530

    fixes

---
+++ script.js
@@ -0,0 +1,79 @@
+// // // console.log("HELLO WORLD");
+
+
+
+// // // var fullName = "Deepjyoti Kalita";
+// // // console.log(fullName);
+// // // let me;
+
+// // // console.log(me);
+
+// // // console.log(undefined >= 0)
+// // // console.log(undefined == 0)
+// // // console.log(undefined >  0)
+// // // console.log(undefined >= null)
+// // // console.log(undefined == null)
+// // // console.log(undefined > null)
+
+// // var rect = document.querySelector("#center");
+
+// // rect.addEventListener("mousemove", function (details) {
+// //    var rectLocation = rect.getBoundingClientRect();
+// //    var rectVal = details.clientX - rectLocation.left;
+
+// //    if(rectVal < rectLocation.width/2) {
+// //         console.log("red");
+// //         var redColor = gsap.utils.mapRange(0,rectLocation.width/2, 255, 0, rectVal);
+// //         gsap.to(rect, {
+// //             backgroundcolor: `rgb(${redColor}, 0, 0)`,
+// //             ease: Power4,
+// //         })
+        
+// //         // console.log(redColor);
+// //    }
+// //    else
+// //    {
+// //     console.log("right");
+// //         //right
+// //    }
+// // })
+
+
+// //  functions----
+
+
+// function  calculateCartPrice(val1, val2, ...num1) {
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200, 300, 400))
+
+// const user = {
+//     username: "deepj",
+//     price: 999
+// }
+
+// function handleObject(reqBody) {
+//     console.log(`username is ${reqBody.usernamey} and price is ${reqBody.price}`);
+// }
+
+// handleObject(user)
+
+// const myArray = [400, 300, 200]
+
+// function getArray(arry ) {
+//     arry.map((el) => {
+//         console.log(el)
+//     })
+// }
+
+// getArray(myArray)
+
+// let a=2
+// let b=3
+// console.log(a, b)
+// a = a ^ b
+// b= a ^ b
+// a= a ^ b
+
+// console.log(a, b)
